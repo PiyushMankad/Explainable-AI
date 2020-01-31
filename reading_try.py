e3cb87f4b352b7369ea4bd8f812eabb2a2ec20e9
Snake_Charmer <piyush@omnipresenttech.com>
2020-01-31 09:38:31 +0000

    Reading a article contents

--- reading_try.py
+++ reading_try.py
@@ -1,23 +1,44 @@
 import json
 
-with open(r"TREC_Washington_Post_collection.v2.jl", encoding="utf8") as fp:
+with open(r"E:\Intelligent Systems\Dissertation ####\TREC_Washington_Post_collection.v2.jl", encoding="utf8") as fp:
 	line = fp.readline()
-	dic = json.loads(line)
-	# print(line)
-	# print((dic))
-	for i,(key,values) in enumerate(dic.items()):
-		print(key,":",values,"\n")
-		try:
-			l = len(dic[key])
-			# print(type(dic[key]),len(dic[key]))
+	count = 1
+	while line:
+		dic = json.loads(line)
+		title = dic['title']
+		
+		## file writing
+		article = open("article{}.txt".format(count),"a+")
+		# article.write(title)
+		content = ""
 
-			for j in range(len(dic[key])):
-				# print(dic[key][0])
-				for (key2,val2) in dic[key][j].items():
-					print(key2,":",val2,"\n")
+		for i,(key,values) in enumerate(dic.items()):
+			## outputs all the key and value pairs
+			# print(key,":",values,"\n")
 
-				print("###############")
+			try:
+				l = len(dic[key])
+				# print(type(dic[key]),len(dic[key]))
 
+				for j in range(len(dic[key])):
+					# print(dic[key][0])
+					
+					for (key2,val2) in dic[key][j].items():
+						if key2 == "content":
+							## outputs all the content keys
+							content = dic[key][j][key2]
+							print(key2,":",val2,"\n")
+							# print("CONTENT",content)
+							# content = content+val2+"\n"
+							# article.write(json.dumps(content),"\n")
 
-		except:
-			pass
+			except:
+				pass
+
+		line = fp.readline()
+		count+=1
+
+		article.close()
+		print("\n\n\n<<<<<<<<########## NEXT ARTICLE {} ############>>>>>>".format(count))
+		if count >=1:
+			break
